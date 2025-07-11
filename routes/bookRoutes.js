981eb8765542e90dbc32c8b8df0680699e982f5e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-11 10:01:25 -0500

    delete route for book

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,11 +2,12 @@
 const express = require("express")
 const router = express.Router();
 
-const { getAllBooks, getBookById, createBook} = require("../controllers/bookController")
+const { getAllBooks, getBookById, createBook, deleteBook} = require("../controllers/bookController")
 
 router.get('/all', getAllBooks);   
 router.get('/:id', getBookById);
 router.post('/create-book', createBook);
+router.delete('/:d', deleteBook);
    
 
 
