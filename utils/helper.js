871337da1b52c5559cc1eebe732b4011d9bf7c35
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-15 14:20:25 -0500

    created read data from file

--- utils/helper.js
+++ utils/helper.js
@@ -1,2 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+
+const booksFile = path.join(__dirname, "../books.json");
+
+function readBooksFromFile() {
+    const data = fs.readFileSync(booksFile, 'utf-8');
+    return JSON.parse(data);
+}
