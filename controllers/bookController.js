6e32a551eca57bf21e4ccb9d48229ccade22f258
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-16 14:51:24 -0500

    added filter search

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,5 @@
 const { uuid } = require("uuidv4");
-const {readBooksFromFile, writeBooksFromFile} = require("../utils/helper")
+const { readBooksFromFile, writeBooksFromFile } = require("../utils/helper")
 
 // var books = [
 //   { id: "1", title: "1984", author: "George Orwell" },
@@ -9,18 +9,31 @@ const {readBooksFromFile, writeBooksFromFile} = require("../utils/helper")
 exports.getAllBooks = (req, res) => {
   try {
     const books = readBooksFromFile();
-    res.json(books)
+
+    const { title, author } = req.query;
+
+    if (!title && !author) {
+      res.json(books)
+    };
+
+    const filteredBooks = books.filter(book => {
+      const matchesTitle = title ? book.title.toLowerCase().includes(title.toLowerCase()) : true
+      const matchesAuthor = author ? book.author.toLowerCase().includes(author.toLowerCase()) : true
+      return matchesTitle && matchesAuthor;
+    });
+    res.json(filteredBooks)
+
   } catch (error) {
     res.json(error)
   }
 }
 
 exports.getBookById = (req, res) => {
-  try {    
+  try {
     const books = readBooksFromFile();
 
     const id = req.params.id;
-  
+
     const book = books.find(b => b.id === id)
     book ? res.json(book) : res.status(404).json({ message: "Book not found! :(" });
 
@@ -56,10 +69,10 @@ exports.updateBook = (req, res) => {
     const id = req.params.id;
     const index = books.findIndex(b => b.id === id);
     console.log("index", index);
-    
 
-    if(index == -1) {
-      return res.status(404).json({message: "Book not found!!"})
+
+    if (index == -1) {
+      return res.status(404).json({ message: "Book not found!!" })
     };
 
     const updatedBooks = {
@@ -72,7 +85,7 @@ exports.updateBook = (req, res) => {
     res.json(updatedBooks)
 
   } catch (error) {
-    res.status(400).json({message:"Something went wrong in book creation", error: error.message})
+    res.status(400).json({ message: "Something went wrong in book creation", error: error.message })
   }
 }
 
@@ -81,12 +94,12 @@ exports.deleteBook = (req, res) => {
     const books = readBooksFromFile();
     const id = req.params.id;
 
-    
+
     const updatedBooks = books.filter(b => b.id !== id);
 
     writeBooksFromFile(updatedBooks);
 
-    res.json({ message: "Book Deleted!", data: updatedBooks});
+    res.json({ message: "Book Deleted!", data: updatedBooks });
 
   } catch (error) {
     res.status(400).json({ message: "Something went wrong in deletion", error: error });
