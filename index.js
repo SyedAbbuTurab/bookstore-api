148e118eee1fed0e9b51fec5210c91cfb23cdc02
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-20 14:34:25 -0500

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     console.log(`${req.url}, ${req.method}`);
     next();
 })
-app.use('/api/books', bookRoutes)
+app.use('/api/books', bookRoutes);
 
 
 module.exports = {
