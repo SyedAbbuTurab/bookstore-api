ace656f62c8adc2b232285f7947fb480a298297c
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-23 20:18:50 -0500

    Added user schema with multiple roles

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    DoB: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: String,
+        enum: ["admin", "operator", "author", "user"],
+        default: "user"
+    },
+    isApproved: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if(this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+}
+
+
