3bcba87364c687e64aa6373f0865f89ab4b0cb96
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-23 21:07:24 -0500

    updated changes with routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("express");
 const cors = require("cors");
 const bookRoutes = require("./routes/bookRoutes");
+const authRoutes = require("./routes/authRoutes");
 const dotEnv = require("dotenv").config();
 
 
@@ -13,6 +14,7 @@ app.use((req, res, next) => {
     next();
 })
 app.use('/api/books', bookRoutes);
+app.use('/api/auth', authRoutes);
 
 
 module.exports = {
