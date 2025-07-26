770222c31b1a1825acf5f840b8cb8a10f231be2b
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-26 14:14:34 -0500

    formatted code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,6 +14,6 @@ const verifyToken = (req, res, next) => {
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({error: "Access denied."})
+        return res.status(401).json({error: "Access denied."});
     }
-}
+};
