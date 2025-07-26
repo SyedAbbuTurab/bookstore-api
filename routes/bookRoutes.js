70fdfd3dd634b286a3312b54ab5511567bfb9f96
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-26 15:12:31 -0500

    Updated routes added to auth verify

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,18 +1,19 @@
 // const router = require("router")
 const express = require("express")
 const router = express.Router();
-const { getAllBooks, getBookById, createBook, updateBook, deleteBook} = require("../controllers/bookController");
+const { getAllBooks, getBookById, createBook, updateBook, deleteBook } = require("../controllers/bookController");
 
 const { bookValidationRoutes } = require("../validators/bookValidator");
 const { validationRequest } = require("../middleware/validateRequest")
 
+const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware")
 
-router.get('/all', getAllBooks);   
+
+router.get('/all', getAllBooks);
 router.get('/:id', getBookById);
 router.put('/:id', updateBook);
-router.post('/create-book', bookValidationRoutes, validationRequest, createBook);
-router.delete('/:id', deleteBook);
-   
+router.post('/create-book', bookValidationRoutes, validationRequest, verifyToken, authorizeRoles('author'), createBook);
+router.delete('/:id', verifyToken, authorizeRoles('admin', 'operator'), deleteBook);
 
 
 module.exports = router
