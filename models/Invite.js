0a27f1abbb43ef1c0d61a0d7526be3f196b4878c
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-27 14:10:21 -0500

    created invitation based DB

---
+++ models/Invite.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const inviteSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['author', 'operator'],
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+})
