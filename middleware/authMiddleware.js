b347d1d298bef136637594414490245cc499e9f3
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-27 14:05:00 -0500

    formatted code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
     
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         res.status(401).json({ error: "No token provided!" })
-    }
+    };
 
     const token = authHeader.split(' ')[1];
 
