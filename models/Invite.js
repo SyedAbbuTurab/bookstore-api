dbde32661dd030f23ac5571bc90b90d788b0dfe2
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-27 14:15:23 -0500

    Invite schema updated with id parameter from _id

--- models/Invite.js
+++ models/Invite.js
@@ -29,4 +29,12 @@ const inviteSchema = new mongoose.Schema({
     }
 });
 
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 module.exports = mongoose.model('Invite', inviteSchema);
