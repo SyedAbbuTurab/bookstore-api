df814a6fbe02c611a741b870c98703ecf961a45a
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-28 03:34:27 -0500

    registered route under server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const server = require("express");
 const cors = require("cors");
 const bookRoutes = require("./routes/bookRoutes");
 const authRoutes = require("./routes/authRoutes");
+const inviteRoutes = require('./routes/inviteRoute')
 const dotEnv = require("dotenv").config();
 
 
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 })
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/invite', authRoutes);
 
 
 module.exports = {
