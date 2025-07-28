de151993848dad228449d2086a3f1b2be0288c56
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-28 03:57:09 -0500

    Updated wronly added controller

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 })
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/invite', authRoutes);
+app.use('/api/invite', inviteRoutes);
 
 
 module.exports = {
