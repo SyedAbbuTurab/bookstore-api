2d6fd4d3aa786de880f5f45a51f6d137f481de49
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-28 04:00:15 -0500

    Added route to handle signup on invitation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const router = express.Router();
 
-const { signUp, logIn } = require("../controllers/authController");
+const { signUp, logIn, signupWithInvite } = require("../controllers/authController");
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
+
+// Route to handle invitation base signup
+router.post("/signup-invite", signupWithInvite);
+
 
 module.exports = router;
