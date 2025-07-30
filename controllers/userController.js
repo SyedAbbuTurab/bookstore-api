27051f5d843fedca473e1e1bbad71fe5f179370f
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-30 12:18:29 -0500

    Added controller for author user management

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require('../models/User');
+
+exports.getPendingAuthors = (req, res) => {
+    try {
+        console.log("Authors state");
+    } catch (error) {
+        console.log(error);
+    }
+}
