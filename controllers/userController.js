07efd46d27433dcc1b84b9fcde82b272ff4897df
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-30 12:54:58 -0500

    Added service to update pendong authors

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,4 +7,24 @@ exports.getPendingAuthors = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'failed to fetch authors', error: error.message });
     }
+};
+
+//PUT call for updating or approving authors.
+exports.approveAuthor = async (req, res) => {
+    try {
+        const { id } = req.body;
+
+        const author = await User.findOne({ _id: id, role: 'author' });
+
+        if (!author) return res.status(404).json({ message: 'Author not found!' });
+        if (author.isApproved) return res.status(400).json({ message: 'Author approved already!' });
+
+        author.isApproved = true;
+        await author.save();
+
+        res.json({ message: 'Author approved successfully', author });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update Author!' })
+    }
 }
