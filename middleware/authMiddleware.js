c895e65d25caaf4ede085b046a556e1be4fb83b9
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-30 15:42:16 -0500

    updated changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;    
     
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         res.status(401).json({ error: "No token provided!" })
@@ -10,18 +10,18 @@ const verifyToken = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);        
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({ error: "Access denied!" });
+        return res.status(401).json({ error: `Access denied! - JWT` });
     };
 };
 
 const authorizeRoles = (...allowedUsers) => {
     return (req, res, next) => {
         if (!req.user || !allowedUsers.includes(req.user.role)) 
-            return res.status(403).json({ error: 'Access denied!' });
+            return res.status(403).json({ error: `Role - Access denied!` });
         next();
     };
 };
