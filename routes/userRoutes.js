64494a708d34032e8d3dcc823ec9f8a15bbb163f
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-07-30 15:42:09 -0500

    updated changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const { authorizeRoles, verifyToken } = require('../middleware/authMiddleware');
 
 const { getPendingAuthors, approveAuthor } = require('../controllers/userController');
 
-router.get('/pending-authors', authorizeRoles('operator'), verifyToken, getPendingAuthors);
+router.get('/pending-authors', verifyToken, authorizeRoles('operator'), getPendingAuthors);
 router.put('/approve-author', authorizeRoles('operator'), verifyToken, approveAuthor);
 
 module.exports = router;
