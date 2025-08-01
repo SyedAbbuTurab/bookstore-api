0828752a22ef42631a18fd370353385db710c82a
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-01 00:04:39 -0500

    added scoped middleware for token verifcation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,12 @@ const { authorizeRoles, verifyToken } = require('../middleware/authMiddleware');
 
 const { getPendingAuthors, approveAuthor, getAllUsers } = require('../controllers/userController');
 
-router.get('/pending-authors', verifyToken, authorizeRoles('operator'), getPendingAuthors);
-router.put('/approve-author', verifyToken, authorizeRoles('operator'),  approveAuthor);
+router.use(verifyToken);
+
+router.get('/pending-authors', authorizeRoles('operator'), getPendingAuthors);
+router.put('/approve-author', authorizeRoles('operator'),  approveAuthor);
 
 // Admin only route to get user based on role or all users.
-router.get('/admin', verifyToken, authorizeRoles('admin'), getAllUsers);
+router.get('/admin', authorizeRoles('admin'), getAllUsers);
 
 module.exports = router;
