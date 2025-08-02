c02a566761864b56adad4f0a26d1103ce016fec6
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-02 01:41:12 -0500

    clean up normal user router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,12 @@ const router = express.Router();
 
 const { authorizeRoles, verifyToken } = require('../middleware/authMiddleware');
 
-const { getPendingAuthors, approveAuthor, getAllUsers, revokeUserPermission } = require('../controllers/userController');
+const { getPendingAuthors, approveAuthor, } = require('../controllers/userController');
 
 router.use(verifyToken);
 
 router.get('/pending-authors', authorizeRoles('operator'), getPendingAuthors);
 router.put('/approve-author', authorizeRoles('operator'),  approveAuthor);
 
-// Admin only route to get user based on role or all users.
-router.get('/admin', authorizeRoles('admin'), getAllUsers);
-router.delete('/admin/:id', authorizeRoles('admin'), revokeUserPermission);
 
 module.exports = router;
