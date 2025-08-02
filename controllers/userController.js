11f9c6ef4a9894b48c582ca99534fc6535596c7e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-02 01:39:58 -0500

    Removed admin controller from regular

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,28 +29,3 @@ exports.approveAuthor = async (req, res) => {
     }
 };
 
-// Admin controller to get all user's
-exports.getAllUsers = async (req, res) => {
-    try {
-        const { role } = req.query;
-        const users = role ? await User.find({ role }) : await User.find();
-        res.json({ message: users })
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong in fetching users!' })
-    }
-};
-
-exports.revokeUserPermission = async (req, res) => {
-    try {
-        const { id } = req.params;
-        
-        const user = await User.findByIdAndDelete(id);
-        
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        };
-        res.json({ message: 'User deleted successfully.' })
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong in deleting user!' })
-    }
-}
