35c82a312178a631d96eb78dc4aad31f303e1e5b
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-04 17:14:47 -0500

    logger

--- utils/logActivity.js
+++ utils/logActivity.js
@@ -1,5 +1,5 @@
 const ActivityLog = require('../models/ActivityLog');
 
 const logActivity = async ({ action, performedBy, target, targetId, meta = {} }) => {
-
+    await ActivityLog.create({ action, performedBy, target, targetId, meta })
 }
