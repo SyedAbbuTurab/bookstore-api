f5defdaad94132524babe5a427e49714ea6ea267
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-04 17:15:38 -0500

    logger error handling

--- utils/logActivity.js
+++ utils/logActivity.js
@@ -1,5 +1,10 @@
 const ActivityLog = require('../models/ActivityLog');
 
 const logActivity = async ({ action, performedBy, target, targetId, meta = {} }) => {
-    await ActivityLog.create({ action, performedBy, target, targetId, meta })
-}
+    try {
+        await ActivityLog.create({ action, performedBy, target, targetId, meta })        
+    } catch (error) {
+        console.error("Something wrong in logger");
+    }
+};
+
