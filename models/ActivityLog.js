f17aca5cd2d64fb6d50778b5a1a849264cd0f9a1
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-04 17:08:31 -0500

    Added methodlogy to remove underscroe

--- models/ActivityLog.js
+++ models/ActivityLog.js
@@ -19,4 +19,12 @@ const activityLogSchema = {
     }
 };
 
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 module.exports = mongoose.Model('ActivityLog', activityLogSchema)
