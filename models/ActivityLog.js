a3eef50c62e0efb4b4cbeb2856c2ab1cb62d73fc
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-04 17:03:34 -0500

    partial model created for activity log

--- models/ActivityLog.js
+++ models/ActivityLog.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
 const activityLogSchema = {
-
+    action: { type: String, required: true },
+    performedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
 };
