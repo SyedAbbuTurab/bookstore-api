678ad5a2360f7418330b43eeeeacb35ffb28b0ad
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-05 12:33:03 -0500

    used proper correct parameter

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -66,7 +66,7 @@ exports.createBook = async (req, res) => {
 
     await logActivity({
       action: 'BOOK_CREATED',
-      performedBy: req.user.role,
+      performedBy: req.user.id,
       target: 'BOOK',
       targetId: newBook.id,
       meta: { title: newBook.title }
