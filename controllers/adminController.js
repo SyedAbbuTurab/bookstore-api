17ba00b57f43bbc4dc2aa7850f27c8ec59cb990d
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-12 10:21:46 -0500

    Added comments for better understanding

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,6 +13,7 @@ exports.getAllUsers = async (req, res) => {
     }
 };
 
+// Revoke or Delete the user iwth permission to add books
 exports.revokeUserPermission = async (req, res) => {
     try {
         const { id } = req.params;
