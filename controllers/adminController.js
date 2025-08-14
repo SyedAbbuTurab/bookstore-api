fb65dfd21cab93540806be265742d30a0cf6802a
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-13 19:13:20 -0500

    added logger for revoke permissions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const { logActivity } = require('../utils/logActivity');
-const {  } = require('../utils/constants')
+const { } = require('../utils/constants')
 
 // Admin controller to get all user's
 exports.getAllUsers = async (req, res) => {
@@ -17,14 +17,21 @@ exports.getAllUsers = async (req, res) => {
 exports.revokeUserPermission = async (req, res) => {
     try {
         const { id } = req.params;
-        
+
         const user = await User.findByIdAndDelete(id);
-        
+
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         };
 
-        
+        await logActivity({
+            action: USER_DELETED,
+            performedBy: req.user.id, // admin
+            target: "User",
+            targetId: deleted._id,
+            meta: { email: deleted.email, role: deleted.role }
+        });
+
         res.json({ message: 'User deleted successfully.' })
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong in deleting user!' })
