6a134a3f63eac857c6da76a0da30db5126bbccf9
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-17 14:06:05 -0500

    token file created

---
+++ utils/tokens.js
@@ -0,0 +1,5 @@
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+exports.signAccessToken = (user) =>
+  jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "15m" });
