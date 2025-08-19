1b71f7ea4c55b98d65fb9c2061be970975521b1e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-18 23:56:49 -0500

    formatted code

--- utils/logActivity.js
+++ utils/logActivity.js
@@ -1,5 +1,6 @@
 const ActivityLog = require('../models/ActivityLog');
 
+// Works as reusabel function to log data to Activity logger
 const logActivity = async ({ action, performedBy, target, targetId, meta = {} }) => {
     try {
         await ActivityLog.create({ action, performedBy, target, targetId, meta })        
