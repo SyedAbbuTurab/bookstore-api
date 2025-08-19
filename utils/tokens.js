a2419f28425678950ad73316a168f18470fa3ffc
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-19 12:04:34 -0500

    refresh value

--- utils/tokens.js
+++ utils/tokens.js
@@ -4,3 +4,5 @@ const crypto = require("crypto");
 exports.signAccessToken = (user) =>
   jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "15m" });
 
+
+exports.generateRefreshValue = () => crypto.randomBytes(40).toString("hex");
