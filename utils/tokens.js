4ddb8bf0c38b78933e856b9657319c7f91f6b86e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-20 14:42:43 -0500

    refresh expiry

--- utils/tokens.js
+++ utils/tokens.js
@@ -1,5 +1,5 @@
-const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 exports.signAccessToken = (user) =>
   jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "15m" });
