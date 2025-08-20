151cd1875b2721a77c3a6e35c53cc54956e00ba9
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-08-20 14:43:09 -0500

    refresh expiry

--- utils/tokens.js
+++ utils/tokens.js
@@ -5,7 +5,7 @@ exports.signAccessToken = (user) =>
   jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "15m" });
 
 
-exports.generateRefreshValue = () => crypto.randomBytes(40).toString("hex");
 exports.hash = (v) => crypto.createHash("sha256").update(v).digest("hex");
+exports.generateRefreshValue = () => crypto.randomBytes(40).toString("hex");
 
 exports.refreshExpiryDate = () => new Date(Date.now() + 1000 * 60 * 60 * 24 * 30); // 30d
